24966a5c3e48bf18b0b5ce692d10eb27ce2c0674
aakash-sachdev <aakashsachdev1992@gmail.com>
2024-01-07 11:01:49 +0530

    <data-and-variable-excercises completed>

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,53 @@
-// Declare and assign the variables below
+// Declare and assign the variables of the data.
 
-// Use console.log to print the 'typeof' each variable. Print one item per line.
+let spaceShuttleName = "Determination";
+let shuttleSpeedMph  = 17500;
+let marsDistanceInKm =	225000000 ;
+let moonDistanceInKm =	384400 ;
+const milesPerKilometer	= 0.621 ; 
 
-// Calculate a space mission below
+// Use console.log to print the 'typeof' each variable.
+console.log(typeof spaceShuttleName) ;
+console.log(typeof shuttleSpeedMph) ;
+console.log(typeof marsDistanceInKm) ;
+console.log(typeof moonDistanceInKm) ;
+console.log(typeof milesPerKilometer) ;
 
-// Print the results of the space mission calculations below
+// Solution to calculate a space mission!
+
+//1) Create and assign a miles to Mars variable.
+let milesToMars = marsDistanceInKm * milesPerKilometer ;
+     
+//2) The hours it would take to get to Mars.
+let hoursToMars = milesToMars / shuttleSpeedMph ; 
+    
+
+//3) Declare a variable and assign it the value of days to Mars.
+let daysToMars = hoursToMars / 24 ;
+    
+
+// Print the results of the space mission calculations below.
+
+    console.log(milesToMars) ;
+    console.log(hoursToMars) ;
+    console.log(daysToMars)  ;
+    console.log(spaceShuttleName,"will take", daysToMars, "days to reach Mars.")
+
 
 // Calculate a trip to the moon below
 
+//Create and assign a miles to Moon variable.
+let milesToMoon = moonDistanceInKm * milesPerKilometer
+
+//The hours it would take to get to Moon.
+let hoursToMoon = milesToMoon / shuttleSpeedMph ;
+
+//the value of days to Moon.
+let daysToMoon = hoursToMoon / 24 ;
+
+
 // Print the results of the trip to the moon below
+    console.log(milesToMoon) ;
+    console.log(hoursToMoon) ;
+    console.log(daysToMoon) ;
+    console.log(spaceShuttleName,"will take", daysToMoon, "days to reach Moon.");
