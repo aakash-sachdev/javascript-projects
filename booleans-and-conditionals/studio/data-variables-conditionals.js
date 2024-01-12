57e8ab1ad2378dfcc903a33734b84631009a3439
aakash-sachdev <aakashsachdev1992@gmail.com>
2024-01-12 08:19:53 +0530

    Studio : SHUTTLE LAUNCH - COMPLETED

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,15 +1,79 @@
 // Initialize Variables below
 
+const date = "Monday 2019-03-18";
+const time = "10:05:34 AM";
+let astronautCount = 7;
+let astronautStatus = "ready";
+let averageAstronautMassKg	= 80.7;
+let crewMassKg = astronautCount * averageAstronautMassKg;
+let fuelMassKg = 760000;
+let shuttleMassKg =	74842.31;
+let totalMassKg =crewMassKg + fuelMassKg + shuttleMassKg;
+const maximumMassLimit =	850000;
+let fuelTempCelsius	= -225;
+const minimumFuelTemp	= -300;
+const maximumFuelTemp	= -150;
+let fuelLevel =	"100%" ;
+let weatherStatus =	"clear" ;
+let preparedForLiftOff = true;
+
+
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
 
-// add logic below to verify all astronauts are ready
+if (astronautCount <= 7) {
+    ("astronautStatus :", astronautStatus )
+    preparedForLiftOff = true;
+} else {
+    ("astronautStatus :","Not", astronautStatus )
+    preparedForLiftOff = false;
+}
 
-// add logic below to verify the total mass does not exceed the maximum limit of 850000
+if (totalMassKg < maximumMassLimit){
+    ( "astronautStatus :", astronautStatus )
+    preparedForLiftOff = true;
+} else {
+    ("astronautStatus :","Not", astronautStatus )
+    preparedForLiftOff = false;
+}
 
-// add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
+if (fuelTempCelsius >= -300 && fuelTempCelsius <= -150){
+    ( "astronautStatus :", astronautStatus )
+    preparedForLiftOff = true;
+}    else {
+        ("astronautStatus :","Not", astronautStatus )
+        preparedForLiftOff = false;
+}
 
-// add logic below to verify the fuel level is at 100%
+if(fuelLevel === "100%"){
+    ( "astronautStatus :", astronautStatus )
+    preparedForLiftOff = true;
+} else {
+    ("astronautStatus :","Not", astronautStatus )
+    preparedForLiftOff = false;
+}
 
-// add logic below to verify the weather status is clear
+if(weatherStatus ==="clear"){
+    ( "astronautStatus :", astronautStatus )
+    preparedForLiftOff = true;
+} else {
+    ("astronautStatus :","Not", astronautStatus )
+    preparedForLiftOff = false;
+}
+let liftOffReady = (astronautStatus === preparedForLiftOff)
 
-// Verify shuttle launch can proceed based on above conditions
+ if (liftOffReady === true)
+ {
+console.log("\nAll systems are ready to go! Initiating space shuttle launch sequence.")
+console.log("date:", date)
+console.log("time:" , time)
+console.log("astronautCount:" ,astronautCount)
+console.log("crewMassKg:", crewMassKg)
+console.log("fuelMassKg:", fuelMassKg)
+console.log("shuttleMassKg:", shuttleMassKg)
+console.log("totalMassKg:", totalMassKg)
+console.log("fuelTempCelsius:",fuelTempCelsius)
+console.log("weatherStatus:", weatherStatus)
+console.log("Wish a safe flight astronauts!")
+ } else{
+    console.log("Mission abort!!!")
+ }
