61b87b1a83e015de1335c83a11665f041c7eb89e
aakash-sachdev <aakashsachdev1992@gmail.com>
2024-01-15 07:25:15 +0530

    Exercise | Completed

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -5,6 +5,25 @@ console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
 
-//Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
+let num1 = String(num);
+let numOfDigits = num1.length;
+
+console.log(`The lenght of num variable is ${numOfDigits}.`);
+
+// Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
+// Modify your code to print out the length of a decimal value EXCLUDING the period.
+
+let decimalNum = 420.19;
+let decimalNum1 = String(decimalNum);
+
+console.log(decimalNum1.slice(1,6).length);
 
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+
+let num2 = 112.098
+
+if (String(num2).includes('.')){
+    console.log(String(num2).length-1);
+    } else {
+    console.log(String(num2).length);
+    }
--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -4,14 +4,31 @@ let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
 
+console.log(language.slice(0,1)+language.slice(4,5));
+
 //2. Without using slice(), use method chaining to accomplish the same thing.
+
+console.log(language.replace('avaScript', 'S'));
 
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
 
+let programming = (language.slice(0,1)+language.slice(4,5));
+console.log(`The abbreviation for '${language}' is '${programming}'.`);
+
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
 
+console.log(language.slice(4).toUpperCase(language).trim());
+//or
+console.log(language.slice(4).toUpperCase(language).indexOf('p'));
+
 //Part Three section Two
+
 
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+
+let firstUpper = notTitleCase.replace("t","T").slice(0,5);
+let lastUpper = notTitleCase.replace("c","C").slice(6,11);
+
+console.log(`${firstUpper} ${lastUpper}`);
