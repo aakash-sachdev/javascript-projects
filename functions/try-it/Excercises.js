7c848f9fb23bbb0c88785d281e437fe89acd095d
aakash-sachdev <aakashsachdev1992@gmail.com>
2024-01-26 05:52:56 +0530

    Excercise | Completed

---
+++ functions/try-it/Excercises.js
@@ -0,0 +1,121 @@
+// Rectangles
+// makeLine(size) function :
+
+function makeLine(size){
+    let columns = "";
+    for (let i = 0; i < size; i++) {
+        columns += "#";
+    } return columns;
+}
+console.log(makeLine(5));
+console.log("----------------------------------------------------------------");
+
+// Square
+// makeSquare(size) function :
+
+function makeSquare(size){
+    
+    let rows = ""; //  if I use \n it is given two lines with some space 
+       for (let j = 0; j < size; j++){
+        rows += makeLine(size);
+        rows += "\n";
+    } 
+    // return rows
+    return rows.slice(0,-1);
+}
+console.log(makeSquare(5));
+console.log("----------------------------------------------------------------");
+
+// makeRectangle(width, height)
+
+function makeRectangle (width,height){
+        let rectangle = "";
+        for (let i = 0; i<height ; i++){
+            rectangle += makeLine(width);
+            rectangle += "\n";
+        }
+        return rectangle.slice(0,-1);   
+}
+
+console.log(makeRectangle(5,3));
+console.log("--------------------------------------------------------------");
+
+//makeDownwardStairs(height)
+
+function makeDownwardStairs(height){
+    let stairs = "";
+    for (let i = 0; i<height; i++){
+    stairs += makeLine(i+1);
+    stairs +="\n" ;
+    }return stairs.slice(0,-1);
+}
+
+console.log(makeDownwardStairs(5));
+console.log("-----------------------------------------------------------------")
+
+// makeSpaceLine(numSpaces, numChars)
+
+function makeSpaceLine(numSpaces, numChars){
+    
+    let spaces = "" ;
+    for ( let i=0; i<numSpaces ;i++) {    
+    spaces += " ";
+    }
+  
+    // for (let i = 0; i < numChars; i++) {
+    //     spaces += "#";
+    // }
+
+    spaces += makeLine(numChars);
+
+
+    for ( let i=0; i<numSpaces ;i++) {    
+        spaces += " ";
+        }
+
+//         spaces += makeLine(numChars - i);
+return spaces;
+ }
+console.log(makeSpaceLine(3,5));
+console.log("-----------------------------------------------------------------");
+
+// makeIsoscelesTriangle(height) function
+
+function makeIsoscelesTriangle(height){
+
+    let triangle = "";
+    for (i = 0; i< height; i++ ){
+    triangle += makeSpaceLine((height - i - 1),(2 * i + 1))+ "\n"
+    }
+    return triangle.slice(0,-1);
+} 
+console.log(makeIsoscelesTriangle(5));
+console.log("-----------------------------------------------------------------");
+
+//Diamonds
+
+
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
+
+function isPalindrome(str) {
+    return reverse(str) === str;
+}
+
+function makeDiamond(height){
+
+    let diamonds="";
+    // for(let i=0; i<height; i++ ){
+    diamonds += makeIsoscelesTriangle(height) +"\n"+ reverse(makeIsoscelesTriangle(height));
+
+    return diamonds;
+}
+    
+    // for(let j = 0; j < height; j++){
+    // diamonds += reverse(makeIsoscelesTriangle(height));
+    // }
+
+
+console.log(makeDiamond (5));
+console.log("-----------------------------------------------------------------");
--- functions/try-it/sayHello.js
+++ functions/try-it/sayHello.js
@@ -1,3 +1,129 @@
 function sayHello() {
    console.log("Hello, World!");
 }
+sayHello();
+
+// let names = ["Aakash", "Mahesh", "varsha", "Emily", "Hitesh"];
+// // for (let i = 0; i < names.length; i++) {
+// //    console.log(names[i]);
+// // }
+// let fruits = ["orange", "apple", "pear"];
+
+// function printArray (names){
+// for (let i=0; i<names.length; i++){
+//    console.log(names[i]);
+//    }
+// }
+// printArray(["Aakash", "Mahesh", "varsha", "Emily", "Hitesh"]);
+// console.log("---");
+// printArray(fruits);
+
+// function sumToN(n){
+//    let sum = 0;
+//    for (let i = 0; i <= n ; i++ ) {
+//       sum += i;
+//    }
+//    return sum;
+// }
+
+// console.log(sumToN(6))
+
+// function countToN(n) {
+//    let count = 1;
+//    while (true) {
+//        if (count > n) {
+//            return;
+//        }
+//        console.log(count);
+//        count++;
+//    }
+// }
+
+// countToN(10)
+
+// function isEven(n) {
+//    return n % 2 === 0;
+// }
+
+
+// isEven(8)
+// console.log(isEven(4));
+// console.log(isEven(7));
+
+// function hello(name) {
+//    return `Hello, ${name}!`;
+// }
+// console.log(hello("aakash"));
+
+// function plusTwo(num) {
+//    return num + 2;
+// }
+
+// let a = 2;
+
+// for (let i=0; i < 4; i++) {
+//    a = plusTwo(a);
+// }
+// console.log(a);
+
+// function repeater(str) {
+//    let repeated = str + str;
+//    console.log(repeated);
+// }
+
+// repeater('Bob');
+
+
+// let message = "Hello, World!";
+// // message = "Goodbye";
+// function printMessage() {
+//     console.log(message);
+// }
+// message = "Goodbye";
+// printMessage();
+// message = "Hello, World!";
+// printMessage();
+
+// let message = "Hello, World!";
+
+// function printMessage(message) {
+//     console.log(message);
+// }
+
+// printMessage("Goodbye");
+
+
+// let num = 42;
+
+// function isEven (num) { 
+//     return num % 2 === 0; 
+// }
+
+// console.log(isEven(43));
+
+// function plusTwo(num) {
+//     return num + 2;
+// }
+
+// let a = 2;
+
+// for (let i=0; i < 4; i++) {
+//     a = plusTwo(a);
+// }
+
+// console.log(a);
+
+function repeater(str) {
+    let repeated = str + str;
+    // console.log(repeated);
+}
+
+console.log(repeater('Bob'));
+
+// let num = 42;
+
+// function isEven (num) {
+//    return num % 2 === 0;
+// }
+
+// console.log(isEven(43));
