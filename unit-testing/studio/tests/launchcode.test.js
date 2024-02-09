b41ad24e834c4b9cb4902c8d6e4e83e432fa95ee
aakash-sachdev <aakashsachdev1992@gmail.com>
2024-02-09 08:12:28 +0530

    Studio | Not Completed

--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -3,6 +3,35 @@ const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
 
-  // Write your unit tests here!
+  test("Value of Organistation", function() {
+    expect(launchcode.organization).toBe("nonprofit");
+  });
+
+  test("Value of executiveDirector", function() {
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  });
   
+  test("Value of percentageCoolEmployees", function() {
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+  });
+  
+  test("Value of programsOffered", function() {
+    expect(launchcode.programsOffered[0]).toBe("Web Development");
+    expect(launchcode.programsOffered[1]).toBe("Data Analysis");
+    expect(launchcode.programsOffered[2]).toBe("Liftoff");
+    expect(launchcode.programsOffered.length).toBe(3);
+  });
+  
+});
+
+describe("Testing launhOutput function", function(){
+
+  test("Num is divisible by 2 ", function() {
+    expect(launchcode.launchOutput(2)).toBe("Launch!");
+  });
+
+  test("Num is divisible by 2 ", function() {
+    expect(launchcode.launchOutput(12)).toBe('Code!');
+  });
+
 });
