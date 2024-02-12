a00b9e79416b072dadaf0eafb049ce883101a8bb
aakash-sachdev <aakashsachdev1992@gmail.com>
2024-02-12 11:22:53 +0530

    Practice

--- arrays/exercises/practice.js
+++ arrays/exercises/practice.js
@@ -1,14 +1,116 @@
-//practice
+// //practice
+
+// let cars = [];
+
+// // Adding
+
+// cars[0] = "Mustang";
+// console.log (typeof(cars));
+// console.log (cars);
+// cars[1] = "Charger";
+// console.log(cars)
+
+// console.log ("**************************************************************************");
+
+// //Index of Element
+
+// console.log (cars[0].indexOf("t"));
+
+// console.log ("**************************************************************************");
+
+// // Replacing
+
+// let plays = ["Othello", "Hamlet"];
+// plays[1] = "King Lear";
+// console.log(plays);
+
+// console.log ("**************************************************************************");
+
+// let books;
+// console.log(typeof(books));
+// // books[0] = "Harry Potter";  // Error - Cuz JS doesn't know Books is an array. 
+
+// console.log ("**************************************************************************");
+
+// let sups = ["Batman", "Spiderman", "Superman", "Ironman", "Thor"];
+// console.log(sups.length);
+
+// console.log ("**************************************************************************");
+
+// let femaleSups = ["Ironlady", "Catwoman", "Supergirl"];
+// femaleSups[0] = "Harley Q";
+// femaleSups[3] = "Scarlet"
+// console.log(femaleSups);
+
+// console.log ("**************************************************************************");
+
+// let sports = ["Football", "Baseball", "Cricket", "Rugby"];
+// console.log(sports.includes("Golf"));
+// console.log(sports.includes("football")); // casesensitive
+// console.log(sports.includes("Football"));
+
+// console.log ("**************************************************************************");
+
+// console.log(sports.indexOf("Cricket"));
+// console.log(sports.indexOf("Golf"))
+// console.log(sports.indexOf("Rugby"))
+
+// console.log ("**************************************************************************");
+
+// console.log(sports.reverse());
+// // console.log(sports.reverse());
+// console.log(sports);
+// console.log(sports.reverse());
+
+// console.log ("**************************************************************************");
+
+// console.log (sports.sort());
+// console.log (sports);
+
+// let nums = [1,4,6,1000,88,2222,19];
+// console.log(nums.sort());
+// console.log(nums);
+
+// console.log ("**************************************************************************");
+
+// let array1 = ["Frasier", "Niles", "Marty", "Daphne", "Roz", "Eddie"];
+
+// array1.splice(2,0,"Bulldog");
+// console.log(array1);
+
+// console.log ("**************************************************************************");
+
+// array1.splice(3,1,"Bulldog");
+// console.log(array1);
+
+// console.log ("**************************************************************************");
+
+// array1.splice(2,2,"Pity");
+// console.log(array1);
+
+// console.log ("**************************************************************************");
+
+// array1.splice(3,2);
+// console.log(array1);
 
 
+let array1 = ["Frasier", "Niles", "Marty", "Daphne", "Roz", "Eddie"];
+let sups = ["Batman", "Spiderman", "Superman", "Ironman", "Thor"];
 
-const school = [
-    ["science", "computer", "art"],
-    ["Jones", "Willoughby", "Rhodes"]
- ];
+console.log ("**************************************************************************");
 
- console.log(school[1][0])
- school[0].push("dance");
-console.log(school);
-school[1].unshift("Holmes");
-console.log(school);
+console.log(array1.slice(1,4));
+console.log(array1);
+
+console.log ("**************************************************************************");
+
+
+array1.splice(2,4,"Bulldog");
+console.log(array1);
+
+console.log ("**************************************************************************");
+
+
+array1.splice(2,4,"Bulldog");
+console.log(array1);
+
