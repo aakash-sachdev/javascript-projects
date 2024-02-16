1ff93f9bddae85ed408cb15e2cb6aec34eaae1d2
aakash-sachdev <aakashsachdev1992@gmail.com>
2024-02-16 07:54:34 +0530

    Studio Done

--- classes/studio/ClassStudio.js
+++ classes/studio/ClassStudio.js
@@ -1,9 +1,46 @@
 //Declare a class called CrewCandidate with a constructor that takes three parameters—name, mass, and scores. Note that scores will be an array of test results.
 
+class CrewCandidate {
+    constructor(name, mass, scores){ 
+       this.name = name;
+       this.mass = mass; //mass is in kg
+       this.scores = scores; // stored in array
+    }
+    addScore(newScore) {
+        this.scores.push(newScore);
+        return this.scores;  
+        }
+
+    avgScore() {
+            let sum = 0;
+            for(let scores of this.scores){
+                sum += scores;
+            }
+        let avg = sum/ this.scores.length;
+        return Math.round(avg*10)/10;  
+    }
+
+    status() {
+        let avgScore = this.avgScore()
+        if (avgScore >= 90) {
+          return "Accepted";
+        } else if (avgScore >= 80 && avgScore <= 89) {
+          return "Reserve";
+        } else if (avgScore >= 70 && avgScore <= 79) {
+          return "Probationary";
+        } else {
+          return "Rejected";
+        }
+    }
+}
 
 
-//Add methods for adding scores, averaging scores and determining candidate status as described in the studio activity.
+ let  bubbaBear = new CrewCandidate('Bubba Bear', 135,  [88, 85, 90]);
+ let merryMaltese = new CrewCandidate('Merry Maltese', 1.5, [93, 88, 97]);
+ let gladGator = new CrewCandidate('Glad Gator', 225, [75, 78, 62]);
+ 
+ 
+ console.log(`${bubbaBear.name} earned an average test score of ${bubbaBear.avgScore()}% and has a status of ${bubbaBear.status()}.`);
+ console.log(`${merryMaltese.name} earned an average test score of ${merryMaltese.avgScore()}% and has a status of ${merryMaltese.status()}.`);
+ console.log(`${gladGator.name} earned an average test score of ${gladGator.avgScore()}% and has a status of ${gladGator.status()}.`);
 
-
-
-//Part 4 - Use the methods to boost Glad Gator’s status to Reserve or higher. How many tests will it take to reach Reserve status? How many to reach Accepted? Remember, scores cannot exceed 100%.
