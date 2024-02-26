0df5bf335fc46ec45ba8c442115c3442824ffe08
aakash-sachdev <aakashsachdev1992@gmail.com>
2024-02-26 16:33:26 -0600

    Pratice

---
+++ objects-and-math/chapter-examples/practiceMath.js
@@ -0,0 +1,37 @@
+//Math Properties:
+console.log(Math.PI);
+console.log(Math.PI*4);
+console.log(Math.PI + Math.PI);
+console.log("************************************************************");
+
+//Math properties are immutable:
+console.log(Math.PI);
+Math.PI = 1234;
+console.log(Math.PI);
+console.log("************************************************************");
+
+//SQRT
+console.log(Math.sqrt(16));
+console.log(Math.abs(-16));
+console.log(Math.ceil(3.6));
+console.log(Math.floor(3.6));
+console.log(Math.pow(3,3));
+console.log(Math.random());
+console.log(Math.random(9));
+console.log(Math.trunc(11.94));
+console.log(Math.SQRT2);
+console.log(Math.floor(-3.87));
+console.log(Math.random(-3.87));
+console.log(Math.round(-3.87));
+console.log(Math.trunc(-3.87));
+console.log("************************************************************");
+
+//Example:
+
+let num = Math.floor(Math.random()*10);
+console.log(num);
+console.log("************************************************************");
+
+let num1 = Math.round(Math.random()*10);
+console.log(num1);
+console.log("************************************************************");
