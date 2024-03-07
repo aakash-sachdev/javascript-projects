d1d4747056ec0b6b80ab7c0ba7df63ef3dfb6217
aakash-sachdev <aakashsachdev1992@gmail.com>
2024-03-06 20:29:51 -0600

    OM

---
+++ dom-and-events/onlineResourceDOM/script.js
@@ -0,0 +1,27 @@
+// console.log("Hello!");
+// console.dir(window.document.body.childNodes);
+// console.log(document.body);
+// console.log(document.head);
+// alert ("Hey there!")
+
+// let heading = document.getElementById("heading");
+// console.log(heading);
+
+// let heading = document.getElementsByClassName("heading");
+// console.log(heading);
+
+// let para = document.getElementsByTagName("p");
+// console.log(para);
+// console.dir(para);
+
+// let ele = document.querySelector("button"); // return the first element
+// console.log(ele);
+// console.dir(ele);
+
+// let eleAll = document.querySelectorAll("button"); // list of node
+// console.dir(eleAll);
+
+// console.log(document.body.firstChild);
+
+let div = document.querySelector("div");
+console.dir(div);
