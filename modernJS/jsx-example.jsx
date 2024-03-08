09881d75107b50d63b7ba79d7c5408c1e9bdcdf8
aakash-sachdev <aakashsachdev1992@gmail.com>
2024-03-07 19:24:02 -0600

    Practice | Modern JS

---
+++ modernJS/jsx-example.jsx
@@ -0,0 +1,6 @@
+{
+    /* JSX Example */
+}
+  
+let title = 'Star Trek: Strange New Worlds';
+let pageHeader = <h1>{title.toUpperCase()}</h1>;
---
+++ modernJS/ts-example.js
@@ -0,0 +1,10 @@
+// TypeScript Example
+var captainPike = "Captain Pike";
+var yearsUntilDisaster = 10;
+var onDuty = true;
+var interests = ["cooking", "horseback riding"];
+console.log(captainPike);
+console.log(yearsUntilDisaster);
+console.log(onDuty);
+interests.forEach(function (interest) { return console.log(interest); });
+// TODO: Transpile into JS with tsc command, then use node to run JS file
